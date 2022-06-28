0575f85f7788c7762f973ed1294a076ef9cd1218
Oscar Bolanos <100238719+ob-instaleap@users.noreply.github.com>
2022-06-28 10:19:19 -0500

    update
    update

--- mynodeserver.js
+++ mynodeserver.js
@@ -3,12 +3,12 @@ const app = express()
 const port = 3001
 
 app.get('/temperature/:location_code', function (request, response) {
-    const varlocation = request.params.location_code;
+   const varlocation = request.params.location_code;
     weather.current(location, function (error, temp_f) {
-        // ..
+        //..
     });
 });
 
 var server = app.listen(port, function () {
-    console.log(`Listening on URL http://localhost:${port}`);
+   console.log(`Listening on URL http://localhost:${port}`);
 })
